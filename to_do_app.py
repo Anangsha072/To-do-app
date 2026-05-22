aa97986078c8ca285c73bc2a4fcb30d45c9cbe6f
Anangsha <146315632+Anangsha072@users.noreply.github.com>
2026-05-22 09:55:43 +0530

    Update to_do_app.py

--- to_do_app.py
+++ to_do_app.py
@@ -4,22 +4,22 @@ parser.add_argument("action")
 parser.add_argument("task", nargs="?")
 args = parser.parse_args()
 if args.action == "add":
-   file = open("tasks1.txt", "a")
+   file = open("tasks.txt", "a")
    file.write(args.task + "\n")
    file.close()
    print("Task added")
 
 elif args.action == "view":
-    file = open("tasks1.txt", "r")
+    file = open("tasks.txt", "r")
     print(file.read())
     file.close()
 
 elif args.action == "delete":
-    file = open("tasks1.txt", "r")
+    file = open("tasks.txt", "r")
     tasks = file.readlines()
     file.close()
     tasks.pop(int(args.task) - 1)
-    file = open("tasks1.txt", "w")
+    file = open("tasks.txt", "w")
     file.writelines(tasks)
     file.close()
     print("Task deleted")
